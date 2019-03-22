7246b67cf2050ae784388440f4812fa776b35b94
David Raygoza <jorgeramos@PG3.local>
2019-03-22 10:14:00 -0600

    My first application with nodeJS

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req, res)=>{
+	res.status = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World');
+});
+server.listen(3000, ()=>{
+	console.log('server on port 3000');
+});
